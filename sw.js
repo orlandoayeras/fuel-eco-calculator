fc78838f9764f0c2024f39871c0cf2c1856f5a5a
orlandoayeras <orlandomark.ayeras@gmail.com>
2026-04-22 13:08:12 +0800

    Added initial files

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'fueleco-v1';
+const BASE = '/fuel-eco-calculator';
+const ASSETS = [BASE + '/', BASE + '/index.html', BASE + '/manifest.json', BASE + '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
