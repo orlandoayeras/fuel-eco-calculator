7e6ea6499c074e2acc59fb4e48452b86442183b3
orlandoayeras <orlandomark.ayeras@gmail.com>
2026-04-22 15:59:49 +0800

    Improved icon and icon mapping. Added .DS_Store in gitignore.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE = 'fueleco-v1';
 const BASE = '/fuel-eco-calculator';
-const ASSETS = [BASE + '/', BASE + '/index.html', BASE + '/manifest.json', BASE + '/icon.svg'];
+const ASSETS = [BASE + '/', BASE + '/index.html', BASE + '/manifest.json', BASE + '/192.png', BASE + '/512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
